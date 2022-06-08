f3e5b8cf39e388370ecd6daa1e58f20eea6e6352
Thanh Duy <99735246+IkariShinji001@users.noreply.github.com>
2022-06-09 00:29:07 +0700

    Add files via upload
    Update auto slider

---
+++ app.js
@@ -0,0 +1,41 @@
+var slider = document.querySelector('.slider');
+var sliderLocation = document.querySelector('.slider-location');
+var sliderSpeech = document.querySelector('.slider-speech');
+
+
+var sliders = [];
+
+sliders[0] = "url('https://www.w3schools.com/w3images/Chicago.jpg')";
+sliders[1] = "url('https://www.w3schools.com/w3images/la.jpg')";
+sliders[2] = "url('https://www.w3schools.com/w3images/ny.jpg')";
+
+var sliderLocations =[];
+
+sliderLocations[0] = "Chicago";
+sliderLocations[1] = "Los Angeles";
+sliderLocations[2] = "New York";
+
+var sliderSpeechs = [];
+ sliderSpeechs[0] = "Thank you, Chicago - A night we won't forget.";
+ sliderSpeechs[1] = "We had the best time playing at Venice Beach!";
+ sliderSpeechs[2] = "The atmosphere in New York is lorem ipsum";
+
+var i = 0;
+
+console.log(sliders.length);
+function changeImg(){
+    slider.style.backgroundImage = sliders[i];
+    sliderLocation.innerHTML = sliderLocations[i];
+    sliderSpeech.innerHTML = sliderSpeechs[i];
+
+    if(i < sliders.length - 1)
+    {
+        i++;
+    }else{
+        i = 0;
+    }
+    console.log(i);
+
+}
+
+setInterval(changeImg, 3000);
